26d2cd1c10757897f512edaa83303593ab88bcd6
alpmieren <alpmieren@gmail.com>
2022-01-11 01:02:54 +0300

    Simpson Book

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,26 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by alperen on 10.01.2022.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init (simpsonName: String, simpsonJob: String, simpsonImage: UIImage) {
+        
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+        
+    }
+    
+}
+
